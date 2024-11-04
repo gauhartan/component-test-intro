13229304af1b85ac89a9cd6e275c785bbe92ee94
unknown <g.tanzharikova@gmail.com>
2024-11-04 08:58:48 +0100

    Initial commit

---
+++ src/App.cy.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import App from './App'
+
+describe('<App />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<App />)
+
+    cy.get('button').click();
+
+    cy.get('button').should('contain', '1');
+
+  })
+})
