cb322551cca6ac30abad2acaaa5f633baf736513
unknown <g.tanzharikova@gmail.com>
2024-11-05 10:20:01 +0100

    Add tests

--- src/App.cy.jsx
+++ src/App.cy.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import App from './App'
 
 describe('<App />', () => {
+
+  beforeEach(() => {
+    cy.mount(<App />)
+  })
+
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<App />)
 
-    cy.get('button').click();
+ //   cy.get('button').click();
 
-    cy.get('button').should('contain', '1');
-
+ //   cy.get('button').should('contain', '1');
   })
+
+  describe('det ska finnas en knapp som visar 0', () => {
+    it('finns en knapp', () => {
+       cy.get('button').should('exist');
+    })
+
+    it('visa 0', () => {
+       cy.get('button').should('contain', '0');
+    })
+  })
+
+  describe('öka med ett om knappen klickas', () => {
+      it('innehåler 1', () => {
+         cy.get('button').click();
+         cy.get('button').should('contain', '1');
+      })
+  })
+
 })
